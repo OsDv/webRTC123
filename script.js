d0484381134c7c6e88b2e3bf00725a550d6b96fc
oussama belguesmia <oussamammm7@gmail.com>
2026-08-03 18:10:25 +0100

    version2 with mqtt

--- script.js
+++ script.js
@@ -1,77 +1,105 @@
 // const mqtt = require('./mqtt/');
-let mqttClient;
+let mqttClient = null;
 const messageInput = document.getElementById("message-input");
 const sendBtn = document.getElementById("send-btn");
 const messageArea = document.getElementById("message-textarea");
-const offerArea = document.getElementById("offer-area");
-const createOfferBtn = document.getElementById("create-offer");
-const receiveOfferBtn = document.getElementById("receive-offer");
-const responseArea = document.getElementById("response-area");
-const createresponseBtn = document.getElementById("create-response");
-const receiveresponseBtn = document.getElementById("receive-response");
-const iceArea = document.getElementById("ice-area");
 const addIceBtn = document.getElementById("add-ice");
-const startConnectionBtn = document.getElementById("start-connection-button");
-
-let username;
-
-responseArea.value = "";
-offerArea.value = "";
-iceArea.value = "";
-startConnectionBtn.addEventListener("click", startConnection);
+const loginBtn = document.getElementById("login-btn");
+const connectionBtn = document.getElementById("connection-button");
+const peerNameInput = document.getElementById("peer-username");
+const passwordInput = document.getElementById("password-input");
+const usernameInput = document.getElementById("username-input");
 const config = {
   iceServers: [
     { urls: "stun:stun1.l.google.com:19302" }
   ]
-}; // Send message function
+};
+let Username = "";
+let isLoggedin = false;
+let isConnected = false;
+let peerName = "";
+messageArea.value = "";
+connectionBtn.addEventListener("click", () => {
+  if (!isConnected) {
+    startConnection();
+  } else {
+    pc.close();
+    console.log("disconecting..");
+  }
+});
+
 sendBtn.addEventListener("click", (e) => {
   const messageContent = messageInput.value;
   messageInput.value = "";
   dataChannel.send(messageContent);
-  console.log("sending messgae: " + messageContent);
-})
-addIceBtn.addEventListener("click", () => {
-  const candidate = new RTCIceCandidate(JSON.parse(iceArea.value));
-  pc.addIceCandidate(candidate).then(() => {
-    console.log("Successfully added ICE candidate");
-  }).catch(e => console.log("Error adding ICE: ", e));
-  iceArea.value = ""; // clear after adding;
+  messageArea.value += Username + ":" + messageContent + '\n';
+  // console.log("sending messgae: " + messageContent);
 })
 
+loginBtn.addEventListener("click", () => {
+  if (!isLoggedin) {
+    const password = passwordInput.value;
+    const username = usernameInput.value;
+    init(username, password);
+    Username = username;
+  } else {
+    passwordInput.value = "";
+    usernameInput.value = "";
+    mqttClient.end();
+    isLoggedin = false;
+    loginBtn.textContent = "login";
+  }
+});
 
 let pc;
 let dataChannel;
-function onCandidate(e) {
-  if (e.candidate != null) {
-    console.log(JSON.stringify(e.candidate));
-  }
-}
+
 function onTrack(e) {
   console.log("got track...");
   console.log(e.streams[0]);
 }
 
-function init() {
+function init(username, password) {
+  // Setup Mqtt (for signaling)
   const options = {
     protocol: 'mqtt',
     host: 'c9c5c8b2dfc14dde8b42bc2d3d1d1ce0.s1.eu.hivemq.cloud',
     port: 8884,
-    username: 'webrtc',
-    password: 'webrtc123'
+    username: username,
+    password: password,
   };
+  if (mqttClient) mqttClient.end();
   mqttClient = mqtt.connect(
     'wss://c9c5c8b2dfc14dde8b42bc2d3d1d1ce0.s1.eu.hivemq.cloud:8884/mqtt',
     {
-      username: 'webrtc',
-      password: 'webrtc123'
+      username: username,
+      password: password,
     }
   );
   mqttClient.subscribe('webrtc');
-  mqttClient.publish('webrtc', 'from javascript');
+  // mqttClient.publish('webrtc', 'from javascript');
   mqttClient.on('message', function(topic, message) {
-    console.log(message.toString());
+    // console.log(message.toString());
     handleMqttMessage(message.toString());
+  });
+  mqttClient.on('connect', function() {
+    isLoggedin = true;
+    loginBtn.textContent = "logout";
   })
+
+  // Set RTCPeerConnection
+  pc = new RTCPeerConnection(config);
+  pc.onicecandidate = onCandidate;
+  pc.addEventListener("connectionstatechange", setOnlineStatus);
+  // dataChannel
+  pc.ondatachannel = (event) => {
+    dataChannel = event.channel;
+    dataChannel.onopen = onDatachannelOpen;
+    dataChannel.onmessage = function(event) {
+      onMessageReceive(event.data);
+      console.log("GOT MESSAGE: " + event.data);
+    };
+  };
 }
 function handleMqttMessage(message) {
   let objectMessage;
@@ -81,47 +109,56 @@ function handleMqttMessage(message) {
     console.log(err);
     return;
   }
-  if (objectMessage.username == username) {
+  if (objectMessage.username == Username) {
     console.log("redundant message...");
     return
   };
   switch (objectMessage.type) {
     case "offer":
-      console.log("Offer received");
+      console.log("Offer received from: " + objectMessage.username);
+      peerName = objectMessage.username;
       acceptOffer(objectMessage.offer);
       break;
     case "candidate":
-      console.log("new candidate received");
-      const candidate = new RTCIceCandidate(objectMessage.candidate);
-      pc.addIceCandidate(candidate).then(() => {
-        console.log("Successfully added ICE candidate");
-      }).catch(e => console.log("Error adding ICE: ", e));
+      console.log("new candidate received from: " + objectMessage.username);
+      try {
+        const candidate = new RTCIceCandidate(objectMessage.candidate);
+        pc.addIceCandidate(candidate).then(() => {
+          console.log("Successfully added ICE candidate");
+        }).catch(e => console.log("Error adding ICE: ", e));
+      } catch (err) {
+        console.log(err);
+      }
       break;
     case "answer":
-      console.log("answer received");
+      console.log("answer received from: " + objectMessage.username);
       acceptAnswer(objectMessage.answer);
       break;
     default:
       break;
   }
 }
-init();
-function onCandidate1(event) {
-  const message = { username: username, type: "candidate", candidate: event.candidate };
+
+function onCandidate(event) {
+  const message = { username: Username, type: "candidate", candidate: event.candidate };
   mqttClient.publish('webrtc', JSON.stringify((message)));
 }
 async function startConnection() {
   try {
-    pc = new RTCPeerConnection(config);
-    pc.onicecandidate = onCandidate1;
+    peerName = peerNameInput.value;
+    if (peerName == "") return;
     dataChannel = pc.createDataChannel("dataChannel");
-    dataChannel.onopen = () => console.log("DATA CHANNEL OPEN!");
+    dataChannel = event.channel;
+    dataChannel.onopen = onDatachannelOpen;
     dataChannel.onmessage = function(event) {
+      onMessageReceive(event.data);
       console.log("GOT MESSAGE: " + event.data);
     };
 
+    messageArea.value = "";
+
     await pc.setLocalDescription();
-    const messageOffer = JSON.stringify({ username: username, type: "offer", offer: pc.localDescription });
+    const messageOffer = JSON.stringify({ username: Username, type: "offer", offer: pc.localDescription });
     mqttClient.publish('webrtc', messageOffer);
   } catch (err) {
     console.log(err);
@@ -129,24 +166,12 @@ async function startConnection() {
 }
 async function acceptOffer(offer) {
   try {
-    pc = new RTCPeerConnection(config);
-    pc.onicecandidate = onCandidate1;
-
-    pc.ondatachannel = (event) => {
-      dataChannel = event.channel;
-      dataChannel.onopen = () => console.log("DATA CHANNEL OPEN!");
-      dataChannel.onmessage = function(event) {
-        console.log("GOT MESSAGE: " + event.data);
-      };
-    };
-
     pc.setRemoteDescription(new RTCSessionDescription(offer));
     // send answer
     const answer = await pc.createAnswer();
     await pc.setLocalDescription(answer);
-    const answerMessage = JSON.stringify({ username: username, type: "answer", answer: answer });
+    const answerMessage = JSON.stringify({ username: Username, type: "answer", answer: answer });
     mqttClient.publish("webrtc", answerMessage);
-
   } catch (err) {
     console.log(err);
   }
@@ -155,97 +180,36 @@ function acceptAnswer(answer) {
   pc.setRemoteDescription(new RTCSessionDescription(answer));
 }
 
-async function createOffer() {
-  try {
-    pc = new RTCPeerConnection(config);
-    pc.onicecandidate = onCandidate;
-    pc.onTrack = onTrack;
+function onMessageReceive(message) {
+  messageArea.value += peerName + ":" + message + '\n';
+}
 
-    dataChannel = pc.createDataChannel("dataChannel");
-    // pc.onicecandidate = (e) =>
-    //   !e.candidate ||
-    //   pc.addIceCandidate(e.candidate).catch(console.log(e.candidate));
-    // const offer = await pc.createOffer();
-    await pc.setLocalDescription();
-
-    // pc.onicegatheringstatechange = () => {
-    //   if (pc.iceGatheringState === "complete") {
-    //     offerArea.value = JSON.stringify(pc.localDescription);
-    //     console.log("Offer ready to copy!");
-    //   }
-    // };
-
-
-    const stringOffer = JSON.stringify(pc.localDescription);
-    offerArea.value = stringOffer;
-    // console.log(stringOffer);
-    console.log(pc);
-  } catch (err) {
-    console.log(err);
+function onDatachannelOpen() {
+  console.log("DATA CHANNEL OPEN!");
+  peerNameInput.value = peerName;
+}
+function setOnlineStatus(status) {
+  switch (pc.connectionState) {
+    case "new":
+    case "connecting":
+      console.log("Connecting…");
+      break;
+    case "connected":
+      isConnected = true;
+      connectionBtn.textContent = "disconnect";
+      console.log("Online");
+      break;
+    case "disconnected":
+      console.log("Disconnecting…");
+      break;
+    case "closed":
+      console.log("Offline");
+      break;
+    case "failed":
+      console.log("Error");
+      break;
+    default:
+      console.log("Unknown");
+      break;
   }
 }
-function readOffer() {
-  pc = new RTCPeerConnection(config);
-  pc.onicecandidate = onCandidate;
-  pc.onTrack = onTrack;
-  // pc.onicecandidate = (e) => {
-  //   !e.candidate ||
-  //     pc.addIceCandidate(e.candidate).catch(console.log("error"));
-  //   console.log(e.candidate);
-  // }
-
-  // dataChannel = pc.createDataChannel("dataChannel");
-  pc.ondatachannel = (event) => {
-    dataChannel = event.channel;
-    dataChannel.onopen = () => console.log("DATA CHANNEL OPEN ON TAB 2!");
-    dataChannel.onmessage = function(event) {
-      console.log("GOT MESSAGE: " + event.data);
-    };
-  };
-
-  const offer = JSON.parse(offerArea.value);
-  pc.setRemoteDescription(new RTCSessionDescription(offer));
-
-  console.log(pc);
-}
-async function createAnswer() {
-  try {
-    const answer = await pc.createAnswer();
-    await pc.setLocalDescription(answer);
-
-
-    // pc.onicegatheringstatechange = () => {
-    //   if (pc.iceGatheringState === "complete") {
-    //     responseArea.value = JSON.stringify(pc.localDescription);
-    //     console.log("Answer ready to copy!");
-    //   }
-    // };
-    //
-
-    const stringAnswer = JSON.stringify(answer);
-    responseArea.value = stringAnswer;
-    // console.log(stringOffer);
-    //
-    console.log(pc);
-  } catch (err) {
-    console.log(err);
-  }
-}
-function readAnswer() {
-  const answer = JSON.parse(responseArea.value);
-  pc.setRemoteDescription(new RTCSessionDescription(answer));
-
-  console.log(pc);
-}
-createOfferBtn.addEventListener("click", () => {
-  createOffer();
-});
-createresponseBtn.addEventListener("click", () => {
-  createAnswer();
-});
-receiveOfferBtn.addEventListener("click", () => {
-  readOffer();
-});
-receiveresponseBtn.addEventListener("click", () => {
-  readAnswer();
-});
